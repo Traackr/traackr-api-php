633853088027afbe158673ce51655f262c84cddf
traack_lcruz <lcruz@traackr.com>
2018-04-18 09:59:16 -0400

    Updating to use same parameter transformations done in standard search end point

--- lib/TraackrApi/Influencers.php
+++ lib/TraackrApi/Influencers.php
@@ -280,18 +280,42 @@ class Influencers extends TraackrApiObject
         $p = $inf->addCustomerKey($p);
         $inf->checkRequiredParams($p, ['audience']);
 
+        $p['keywords'] = is_array($p['keywords']) ?
+            implode(',', $p['keywords']) : $p['keywords'];
+
         if (isset($p['influencers'])) {
             $p['influencers'] = is_array($p['influencers']) ?
                 implode(',', $p['influencers']) : $p['influencers'];
         }
 
         if (isset($p['tags'])) {
-            $p['tags'] = is_array($p['tags']) ? implode(',', $p['tags']) : $p['tags'];
+            $p['tags'] = is_array($p['tags']) ?
+                implode(',', $p['tags']) : $p['tags'];
         }
 
         if (isset($p['tags_exclusive'])) {
             $p['tags_exclusive'] = is_array($p['tags_exclusive']) ?
                 implode(',', $p['tags_exclusive']) : $p['tags_exclusive'];
+        }
+
+        if (isset($p['exclusion_keywords'])) {
+            $p['exclusion_keywords'] = is_array($p['exclusion_keywords']) ?
+                implode(',', $p['exclusion_keywords']) : $p['exclusion_keywords'];
+        }
+
+        if (isset($p['root_urls_inclusive'])) {
+            $p['root_urls_inclusive'] = is_array($p['root_urls_inclusive']) ?
+                implode(',', $p['root_urls_inclusive']) : $p['root_urls_inclusive'];
+        }
+
+        if (isset($p['root_urls_exclusive'])) {
+            $p['root_urls_exclusive'] = is_array($p['root_urls_exclusive']) ?
+                implode(',', $p['root_urls_exclusive']) : $p['root_urls_exclusive'];
+        }
+
+        if (isset($p['emails'])) {
+            $p['emails'] = is_array($p['emails']) ?
+                implode(',', $p['emails']) : $p['emails'];
         }
 
         return $inf->post(TraackrApi::$apiBaseUrl . 'influencers/search/audience', $p);
